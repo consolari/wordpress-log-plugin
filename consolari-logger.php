03fd0f8289d9357a0048a992bcc9907d5e1bc226
consolari <peter@consolari.io>
2014-12-23 20:56:32 +0100

    added sql log

--- consolari-logger.php
+++ consolari-logger.php
@@ -17,43 +17,222 @@ class ConsolariHelper
 {
     private $logger;
 
+    private $options;
+
     public function __construct()
     {
         require __DIR__.'/php-logger/autoload.php';
 
         /*
+         * Get custom options from backend settings
+         */
+        $this->options = get_option('consolari-options');
+
+        if (empty($this->options['key']) or empty($this->options['user'])) {
+            return;
+        }
+
+        /*
          * Initiate session
          */
         $this->logger = new \Consolari\Logger();
-        $this->logger->setKey('b65b79d9e047d97cdd117bdfd46d3944');
-        $this->logger->setUser('peter@consolari.io');
+        $this->logger->setKey($this->options['key']);
+        $this->logger->setUser($this->options['user']);
         $this->logger->setSource($_SERVER['HTTP_HOST']);
         $this->logger->setLevel('message');
         $this->logger->setUrl($_SERVER['REQUEST_URI']);
     }
 
-    public function test()
+
+    public function __destruct()
     {
-        /*
-         * Add one entry that logs the _SERVER array
-         */
-        $entry = new Consolari\Entries\ArrayEntry();
-        $entry->setValue($_SERVER);
-        $entry->setGroupName('Environment');
-        $entry->setLabel('SERVER');
+        if (empty($this->logger)) {
+            return;
+        }
 
-        $this->logger->addEntry($entry);
+        /*if(empty($this->log_sql)){
+            $this->log_sql = 'No queries logged';
+        }*/
 
-        /*
-         * Setup the transport layer
-         */
+        /*$file = $_SERVER['DOCUMENT_ROOT'].'/logs/mysql/mysql_log.txt';
+
+        if(file_exists($file)){
+            $this->logGroup('server', '<div style="margin:10px;">'.$this->reverse_log($file).'</div>', 'MySQL ERRORS');
+        }*/
+
+        $this->log('server', !empty($_SESSION)?$_SESSION:array(), 'SESSION');
+        $this->log('server', !empty($_POST)?$_POST:'no post', 'POST');
+        $this->log('server', !empty($_GET)?$_GET:'no get', 'GET');
+        $this->log('server', !empty($_REQUEST)?$_REQUEST:'no request', 'REQUEST');
+        $this->log('server', !empty($_FILES)?$_FILES:'no files', 'FILES');
+        $this->log('server', $_COOKIE, 'COOKIE');
+        $this->log('server', $_SERVER, 'SERVER', 'array');
+//        $this->log('server', self::convertArrayToTable($exceptions, array('Datetime', 'Type', 'Message', 'URI')), 'EXCEPTION LOG', 'table');
+
+        $this->setQueries();
+
         $transport = new Consolari\Transport\Curl();
+
         $this->logger->setTransport($transport);
         $this->logger->send();
     }
 
+    private function setQueries()
+    {
+        global $wpdb;
+
+        if (!empty($wpdb->queries)) {
+            foreach ($wpdb->queries as $query) {
+
+                $entry = new Consolari\Entries\Query();
+                $entry->setSql($query[0]);
+                $entry->setGroupName('SQL Queries');
+                $entry->setLabel('SQL');
+
+                if (isset($query['trace'])) {
+                    $trace = $query['trace']->get_trace();
+
+                    /*$context = self::getContext($trace);
+
+                    if (!empty($context)) {
+                        $entry->setContext($context);
+                    }*/
+                }
+
+                $this->logger->addEntry($entry);
+            }
+        }
+    }
+
+    public function init()
+    {
+        die('test');
+    }
+
+    public function log($groupName = '', $data = '', $label = 'Data', $dataType = 'none')
+    {
+        if (empty($this->logger)) {
+            return;
+        }
+
+        $trace = debug_backtrace();
+
+        $contextData = self::getContext($trace);
+        unset($trace);
+
+        $context = new \Consolari\Context\Context();
+        $context->setFile($contextData['file']);
+        $context->setClass($contextData['class']);
+        $context->setMethod($contextData['method']);
+        $context->setLine($contextData['line']);
+        $context->setCode($contextData['code']);
+        $context->setLanguage($contextData['language']);
+        unset($contextData);
+
+        if ($dataType == 'none') {
+            if (is_array($data)) {
+                $dataType = 'array';
+            } else {
+                $dataType = 'string';
+            }
+        }
+
+        if ($dataType == 'array' and !is_array($data)) {
+            $dataType = 'string';
+        }
+
+        switch ($dataType) {
+            default:
+                throw new Exception('Unknown datatype '.$dataType);
+                break;
+            case 'array':
+                $entry = new Consolari\Entries\ArrayEntry();
+
+                if (is_array($data)) {
+                    $entry->setValue($data);
+                }
+                break;
+            case 'url':
+            case 'string':
+                $entry = new Consolari\Entries\String();
+                $entry->setValue($data);
+                break;
+            case 'html':
+            case 'xml':
+                $entry = new Consolari\Entries\String();
+                $entry->setValue($data);
+                $entry->setContentType(Consolari\Entries\EntryContentType::XML);
+                break;
+            case 'json':
+                $entry = new Consolari\Entries\String();
+                $entry->setValue($data);
+                $entry->setContentType(Consolari\Entries\EntryContentType::JSON);
+                break;
+            case 'table':
+                $entry = new Consolari\Entries\Table();
+                $entry->setValue($data);
+                break;
+        }
+
+        $entry->setGroupName($groupName);
+        $entry->setLabel($label);
+        $entry->setContext($context);
+
+        $this->logger->addEntry($entry);
+    }
+
+
+    /**
+     * Get contect of log requestor
+     *
+     * @param array $trace
+     */
+    public function getContext($trace, $level = 0)
+    {
+        $contextLines = 8;
+        $ignoreClasses = array();
+
+        $context = array(
+            'file'=>'',
+            'line'=>0,
+            'class'=>'',
+            'method'=>'',
+        );
+
+        for ($i = $level; $i<$level+10; $i++) {
+            if (isset($trace[$i+1]['class']) and !in_array($trace[$i+1]['class'], $ignoreClasses)) {
+                $context = array(
+                    'file'=>$trace[$i]['file'],
+                    'line'=>$trace[$i]['line'],
+                    'class'=>$trace[$i+1]['class'],
+                    'method'=>$trace[$i+1]['function'],
+                );
+
+                break;
+            }
+        }
+
+        if (file_exists($context['file'])) {
+            $code = file($context['file']);
+        } else {
+            $code = array();
+        }
+
+        $codeStr = '';
+        for ($i=$context['line']-$contextLines; $i < $context['line']+$contextLines; $i++) {
+
+            if (isset($code[$i])) {
+                $codeStr .= $code[$i];
+            }
+        }
+
+        $context['code'] = $codeStr;
+        $context['language'] = 'php';
+
+        return $context;
+    }
 }
 
+//if ( is_admin() or is_user_logged_in()) {
 $logger = new ConsolariHelper();
-$logger->test();
-
+//}
--- src/admin_menu.php
+++ src/admin_menu.php
@@ -4,20 +4,68 @@
  * Limit to admin user
  */
 if (is_admin()) {
-    add_action( 'admin_menu', 'consolari_plugin_menu' );
-    add_action( 'admin_init', 'consolari_register_settings' );
-}
- 
-function consolari_register_settings()
-{
-    register_setting( 'consolari-group', 'Email (username)' );
-    register_setting( 'consolari-group', 'Key' );
+    add_action('admin_menu', 'consolari_plugin_menu');
+    add_action('admin_init', 'consolari_admin_init');
 }
 
-function consolari_plugin_menu() {
+/**
+ * Admin init callback
+ */
+function consolari_admin_init()
+{
+    register_setting( 'consolari-options', 'consolari-options', 'consolari_options_validate' );
+
+    add_settings_section('consolari_main', 'Account settings', 'consolari_section_text', 'consolari_plugin_options');
+
+    add_settings_field('consolari_setting_user', 'User', 'consolari_setting_user', 'consolari_plugin_options', 'consolari_main');
+    add_settings_field('consolari_setting_key', 'Key', 'consolari_setting_key', 'consolari_plugin_options', 'consolari_main');
+}
+
+/**
+ * Section main text callback
+ */
+function consolari_section_text()
+{
+    echo '<p>Enter credentials here from Consolari account.</p>';
+}
+
+/**
+ * Callback to generate text input
+ */
+function consolari_setting_user()
+{
+    $options = get_option('consolari-options');
+    echo "<input id='plugin_text_string' name='consolari-options[user]' size='40' maxlength='100' type='text' value='{$options['user']}' />";
+}
+
+function consolari_setting_key()
+{
+    $options = get_option('consolari-options');
+    echo "<input id='plugin_text_string' name='consolari-options[key]' size='40' maxlength='100' type='text' value='{$options['key']}' />";
+}
+
+/**
+ * Callback to validate input
+ *
+ * @param $input
+ * @return mixed
+ */
+function consolari_options_validate($input)
+{
+    return $input;
+}
+
+/**
+ * Callback to build admin menu in settings
+ */
+function consolari_plugin_menu()
+{
     add_options_page( 'Consolari Options', 'Consolari Debug Logger', 'manage_options', 'consolari_plugin_options', 'consolari_plugin_options' );
 }
 
+/**
+ * Callback to build settings form
+ */
 function consolari_plugin_options() {
     if ( !current_user_can( 'manage_options' ) )  {
         wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
@@ -29,29 +77,12 @@ function consolari_plugin_options() {
         <h2>Consolari Debug Logger settings</h2>
 
         <form method="post" action="options.php">
-            <?php settings_fields( 'baw-settings-group' ); ?>
-            <?php do_settings_sections( 'baw-settings-group' ); ?>
-            <table class="form-table">
-                <tr valign="top">
-                    <th scope="row">New Option Name</th>
-                    <td><input type="text" name="new_option_name" value="<?php echo esc_attr( get_option('new_option_name') ); ?>" /></td>
-                </tr>
-
-                <tr valign="top">
-                    <th scope="row">Some Other Option</th>
-                    <td><input type="text" name="some_other_option" value="<?php echo esc_attr( get_option('some_other_option') ); ?>" /></td>
-                </tr>
-
-                <tr valign="top">
-                    <th scope="row">Options, Etc.</th>
-                    <td><input type="text" name="option_etc" value="<?php echo esc_attr( get_option('option_etc') ); ?>" /></td>
-                </tr>
-            </table>
+            <?php settings_fields( 'consolari-options' ); ?>
+            <?php do_settings_sections( 'consolari_plugin_options' ); ?>
 
             <?php submit_button(); ?>
 
         </form>
-
     </div>
 
     <?php
